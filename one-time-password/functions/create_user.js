932cdb389e4a72984f32ee0dd83100b649f9ac39
Bruno Martinez <bruno.martinez@decemberlabs.com>
2021-05-02 15:29:17 -0600

    First firebase deploy request one time password cloud function

---
+++ one-time-password/functions/create_user.js
@@ -0,0 +1,13 @@
+const admin = require("firebase-admin");
+
+module.exports = function(req, res) {
+  if (!req.body.phone) {
+    return res.status(422).send({error: "You must provide a phone number"});
+  }
+
+  const userPhone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  admin.auth().createUser({uid: userPhone})
+      .then((user) => res.send(user))
+      .catch((err) => res.status(422).send({error: err}));
+};
