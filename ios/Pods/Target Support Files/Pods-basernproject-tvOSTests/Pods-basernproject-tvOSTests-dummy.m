36395c66480d2f9b3c38fe845760db03df8974b5
Bruno Martinez <brunociompi@gmail.com>
2021-07-11 16:41:46 -0300

    Xcode12.5 support (#10)

--- ios/Pods/Target Support Files/Pods-basernproject-tvOSTests/Pods-basernproject-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_basernproject_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_basernproject_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-basernproject/Pods-basernproject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_basernproject : NSObject
-@end
-@implementation PodsDummy_Pods_basernproject
-@end
